9073b5614f54298157473dc8cd7ec95ae6c562b3
Meron <meron@augmedix.com>
2020-06-27 11:20:14 -0700

    initial commit/installed chart.js and react bootstrap packages

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import loanData from './loanData';
+import { Navbar, Nav } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+  console.log(loanData)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbar collapseOnSelect bg="light" id="navbar" expand="lg">
+      <Navbar.Brand href="#home">
+        <img className="navbar__brand-img" src="https://uploads-ssl.webflow.com/5e067beb4c88a64e31622d4b/5e20ace96e02156e0600fa1f_favicon-webclip.png"></img>
+      </Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link className="navbar__link" href="#dashboard">Dashboard</Nav.Link>
+          <Nav.Link className="navbar__link" href="#link">Saved Plots</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
---
+++ src/app/components/dashboard/Dashboard.js
@@ -0,0 +1,5 @@
+class Dashboard extends React.Component {
+  render() {
+
+  }
+}
